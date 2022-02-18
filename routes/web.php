ad65d36f03c39422261ab1539593d3212a328659
aleksejem <alexeyhong10@gmail.com>
2022-02-18 12:03:49 +0500

    Added dump routes for handling activation/verifying requests.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('activate', function () {
+    return view('welcome');
+});
+
+Route::post('verify/{envato_user_id}', function () {
+    return view('welcome');
+});
