0b47e45ef31786d6a51a66a4a32f34bc2421f139
aleksejem <alexeyhong10@gmail.com>
2022-02-22 12:17:32 +0500

    Added composer phar.

---
+++ scripts/deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started ..."
+
+#php = '/opt/php80/bin/php'
+php = 'php'
+# Enter maintenance mode or return true
+# if already is in maintenance mode
+($php artisan down) || true
+
+
+# Pull the latest version of the app
+git pull origin production
+
+# Install composer dependencies
+$php ./composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
+# Clear the old cache
+$php artisan clear-compiled
+
+# Recreate cache
+$php artisan optimize
+
+# Compile npm assets
+npm run prod
+
+# Run database migrations
+$php artisan migrate --force
+
+# Exit maintenance mode
+$php artisan up
+
+echo "Deployment finished!"
