2aa1aa59abdd67a213a0a0b0853a208c4d5d001c
aleksejem <alexeyhong10@gmail.com>
2022-03-01 16:29:13 +0500

    Updated .envs files #3.

--- app/Services/Activation/EnvatoPluginActivation.php
+++ app/Services/Activation/EnvatoPluginActivation.php
@@ -2,15 +2,38 @@
 
 namespace App\Services\Activation;
 
+use App\Models\User;
 use App\Services\Interfaces\OAuthInterface;
 use App\Services\Interfaces\PluginActivationInterface;
+use Illuminate\Support\Facades\Http;
 
 class EnvatoPluginActivation implements PluginActivationInterface
 {
     public function activate(OAuthInterface $auth)
     {
-        dd($auth->getUser());
+        $user = $auth->getUser();
+
+        $user = User::firstOrCreate([
+            'email' => $user['email'],
+            'name' => $user['name'],
+            'nickname' => $user['nickname'],
+            'access_token' => $user['token'],
+            'refresh_token' => $user['refreshToken'],
+            'password' => bcrypt(123)
+        ]);
+
+        $userPurchasedItems = Http::withToken($user->access_token)
+                ->post($this->getUserPurchaseListApiUrl())
+                ->json();
+
+        dd($userPurchasedItems);
+
         ///handle
         /// create record of user
+    }
+
+    protected function getUserPurchaseListApiUrl(): string
+    {
+        return config('envato.envato_api_url') . 'market/buyer/list-purchases';
     }
 }
